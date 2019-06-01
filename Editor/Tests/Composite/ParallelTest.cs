702244d5e62da1d01c5a6712f0108373db8d3062
glitch <code@glitch.land>
2019-05-31 21:46:39 -0600

    Fix an editor warning in 2019.1.1f1
    Fix variable 'e' declared and not used.

--- Editor/Tests/Composite/ParallelTest.cs
+++ Editor/Tests/Composite/ParallelTest.cs
@@ -293,7 +293,7 @@ namespace NPBehave
                 
                 sut.StopLowerPriorityChildrenForChild(firstChild, false);
             }
-            catch(Exception e)
+            catch
             {
                 bExceptionCought = true;
             }
